2920d09a9188f02e0afd1335d9c5489e77567aa7
biancacosaj <biancarubionotario@gmail.com>
2019-04-01 15:23:22 +0200

    Added scroll to top on survey page.

--- assets/js/survey.js
+++ assets/js/survey.js
@@ -5,41 +5,37 @@ $(document).ready(function(){
     radioClass: 'iradio_square-blue',
   });
 
-   
   
-  $( "#js-form-button" ).on( 'click', function() {
-    var radioList = $('input[type=radio]');
+  $( "#js-form-button" ).click(function( event ) {
+      event.preventDefault();
+      $("html, body").animate({ scrollBottom: $($(this).attr(href="goup")).offset().bottom }, 500);
+  });
+  
+  
+  $( "#js-form-button" ).on( 'click', function(){
     var radioCheckedTrue = 0;
     var radioChecked = 0;
 
-    radioList.each(function (index, radio){
-      if (radio.checked) 
-      radioChecked += 1
+    $('input[type=radio]').each(function (index, radio){
+      if (radio.checked){
+        radioChecked += 1
+      }
         
       if (radio.checked && radio.value == "1"){
         radioCheckedTrue += 1 
       }
-    });
+    }); 
 
-    if(radioChecked == 14 && radioCheckedTrue == 14)
+    if(radioChecked == 14 && radioCheckedTrue == 14){
       window.location = "privacy-safe.html"
-    if(radioChecked < 14)
-      alert("Deben estar todas las preguntas respondidas.")
-    if(radioChecked == 14 && radioCheckedTrue < 14)
-      window.location = "privacy-invaded.html"
-    console.log(checked)
+    } else if(radioChecked < 14){
+       alert("Deben estar todas las preguntas respondidas.")
+    } else (radioChecked == 14 && radioCheckedTrue < 14);{
+       window.location = "privacy-invaded.html"
+    }
+     console.log(checked)
     
   });
+
   
-   
 });
-
-
-
-
-
- 
-  
-
-
-
