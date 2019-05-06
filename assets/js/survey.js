d45f945af46e8870839da0890e011a598aeb6ab0
biancacosaj <biancarubionotario@gmail.com>
2019-05-06 16:54:26 +0200

    remove translations initialization duplicated

--- assets/js/survey.js
+++ assets/js/survey.js
@@ -1,68 +1,45 @@
 /*
 	  Hecho con mucho amor por Devscola y Nolegaltech <3
 */
-$(document).ready(function(){
-
-    initializeICheck();
-
-    initializeSubmitButton();
-
-    initializeTranslate();
+$(document).ready(function () {
+  initializeICheck();
+  initializeSubmitButton();
 });
 
- 
-function initializeTranslate(){
-    var translator = $('body').translate({lang: "es", t: transDictionary});
-    var str = translator.g("translate");
-
-    addChangeLangListener(function(lang) {
-      translator.lang(lang);
-    })
-};
-
-function addChangeLangListener(translate) {
-  $("#jq-dropdown").click(
-    function(event) {
-      event.preventDefault();
-      var lang = event.target.id
-      translate(lang);
+function initializeICheck() {
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_square',
+    radioClass: 'iradio_square-blue',
   });
-}
-
-function initializeICheck(){
-    $('input').iCheck({
-        checkboxClass: 'icheckbox_square',
-        radioClass: 'iradio_square-blue',
-    });
 };
 
-function initializeSubmitButton(){
-  $( "#submit-button" ).on( 'click', function(){
+function initializeSubmitButton() {
+  $("#submit-button").on('click', function () {
     const allTheQuestions = 14;
     var radioCheckedTrue = 0;
     var radioChecked = 0;
-    
+
     var url = window.location.href.split('#');
     var lang = url[url.length - 1];
     console.log(lang);
 
-    $('input[type=radio]').each(function (index, radio){
-      if (radio.checked){
+    $('input[type=radio]').each(function (index, radio) {
+      if (radio.checked) {
         radioChecked += 1
       }
-        
-      if (radio.checked && radio.value == "1"){
-        radioCheckedTrue += 1 
-      }
-    }); 
 
-    if(radioChecked == allTheQuestions && radioCheckedTrue == allTheQuestions){
+      if (radio.checked && radio.value == "1") {
+        radioCheckedTrue += 1
+      }
+    });
+
+    if (radioChecked == allTheQuestions && radioCheckedTrue == allTheQuestions) {
       window.location = "privacy-safe.html"
-    } else if (radioChecked < allTheQuestions){
-        alert("Deben estar todas las preguntas respondidas.")
-    } else if (radioChecked == allTheQuestions && radioCheckedTrue < allTheQuestions){
-        window.location = "privacy-invaded.html"
+    } else if (radioChecked < allTheQuestions) {
+      alert("Deben estar todas las preguntas respondidas.")
+    } else if (radioChecked == allTheQuestions && radioCheckedTrue < allTheQuestions) {
+      window.location = "privacy-invaded.html"
     }
-  }); 
+  });
 
 }
