0eba437e1ac9738c6fa7588ca9631cf4416f7b27
biancacosaj <biancarubionotario@gmail.com>
2019-05-06 17:14:37 +0200

    store and get the chosen lang

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
     initializeTranslate();
+    addStoreLangListener();
 });
 
 function initializeTranslate() {
-    var translator = $('body').translate({ lang: "es", t: transDictionary });
-    var str = translator.g("translate");
+    var currentLang = getLang();
+    var translator = $('body').translate({ lang: currentLang, t: transDictionary });
 
     addChangeLangListener(function (lang) {
         translator.lang(lang);
-    })
+    });
 };
 
 function addChangeLangListener(translate) {
@@ -18,4 +19,15 @@ function addChangeLangListener(translate) {
             var lang = event.target.id
             translate(lang);
         });
+}
+
+function addStoreLangListener(){
+    $("#jq-dropdown").click(function (event) {
+        event.preventDefault();
+        localStorage.setItem('lang', event.target.id);
+    });
+}
+
+function getLang(){
+    localStorage.getItem('lang') || navigator.language;
 }
